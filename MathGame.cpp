0449d584f507009a11a276b0013577c49a39aec8
Ahmed <135174947+uaxanenemos5874@users.noreply.github.com>
2026-05-18 05:20:39 +0100

    MathGame

---
+++ MathGame.cpp
@@ -0,0 +1,490 @@
+// This is Math game.
+// Date: 17/05/2025, Time: 21:50
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+enum enLevelGame {Easy = 1, Medium = 2, Hard = 3, Mix = 4};
+enum enOperationType {Add = 1, Subtraction = 2, Multiplication = 3, Division = 4, MiX= 5};
+
+struct stInfoGame {
+
+    short Question = 0;
+    short FirstNumber = 0;
+    short SecondNumber = 0;
+    short Sum = 0;
+    short AnswerUser = 0;
+    enLevelGame Level;
+    enOperationType Operator;
+    bool Answer;
+
+};
+
+struct stInfoResultGame {
+
+    short NumberOfQuestion = 0;
+    string NameQuestionLevel;
+    string NameOpType;
+    short NumberOfRightAnswers = 0;
+    short NumberOfWrongAnswer = 0;
+    bool Pass;
+
+};
+
+
+
+
+// This Numbers Questions.
+int HowToManyQus () { 
+
+    int manyQus = 0;
+    do {
+    cout << "How Many Questions do you want to answer ?";
+    cin >> manyQus;
+    }
+    while(!manyQus);
+
+    return manyQus;
+
+} 
+
+enLevelGame ReadChiosUserLevel () {
+
+    short Chios;
+
+    do {
+
+        cout << "Enter Question Level [1] Easy, [2] Med, [3] Hard, [4] Mix ?";
+        cin >> Chios;
+    
+    }
+    while (Chios < 1 || Chios > 4);
+
+    return (enLevelGame)Chios;
+
+}
+
+enOperationType ReadChiosUserOperationType () {
+
+    short Chios;
+
+    do {
+
+        cout << "Enter Question Level [1] Addition, [2] Subtraction, [3] Multiplication, [4] Division, [5] Mix ? ";
+        cin >> Chios;
+    
+    }
+    while (Chios < 1 || Chios > 5);
+
+    return (enOperationType)Chios;
+
+}
+
+string LevelName (enLevelGame Chios) {
+
+   string arrLevelGame[4] = {"Easy", "Medium", "Hard", "Mix"}; 
+
+   return arrLevelGame[Chios - 1];
+
+}
+
+string OperatorTypeName (enOperationType Chios) {
+
+    string arrOperationType[5] = {"Addition", "Subtraction", "Multiplication", "Division", "Mix"};
+    
+    return arrOperationType[Chios - 1];
+
+}
+
+int Random_Number (int From, int To) {
+
+    int RandomNumber;
+    RandomNumber = rand () % (To - From + 1) + From;
+
+    return RandomNumber;
+
+}
+
+int NumberLevel (enLevelGame Chios) {
+
+
+
+    switch (Chios) {
+
+        case Easy:
+        return Random_Number(1, 10);
+        break;
+
+        case Medium:
+        return Random_Number(10, 50);
+        break;
+
+        case Hard:
+        return Random_Number(50, 100);
+        break;
+
+        case Mix:
+        return Random_Number(1, 100);
+        break;
+    
+    }
+
+    return 1;
+
+
+}
+
+void PrintQuestion (enOperationType OP, short FirstNumber, short SecondNumber) {
+
+    char arrOP[4] = {'+', '-', '*', '/'};
+
+    if (OP == enOperationType::Add) {
+
+        cout << FirstNumber << endl << SecondNumber << " + " << endl;
+        cout << endl << "________________" << endl;
+
+
+    }
+    else if (OP == enOperationType::Subtraction) {
+
+        cout << FirstNumber << endl << SecondNumber << " - " << endl;
+        cout << endl << "________________" << endl;
+
+
+    }
+    else if (OP == enOperationType::Multiplication) {
+
+        cout << FirstNumber << endl << SecondNumber << " * " << endl;
+        cout << endl << "________________" << endl;
+
+
+    }
+    else if (OP == enOperationType::Division) {
+
+        cout << FirstNumber << endl << SecondNumber << " / " << endl;
+        cout << endl << "________________" << endl;
+
+
+    }
+    else {
+        
+        cout << FirstNumber << endl << SecondNumber << arrOP[Random_Number(1, 4) - 1] << endl;
+        cout << endl << "________________" << endl;
+
+    
+    }
+
+
+}
+
+bool PassOrFail (short NumberOfRight, short NumberOfWrong) {
+
+    if (NumberOfRight > NumberOfWrong || NumberOfRight == NumberOfWrong)
+    return true;
+    else
+    return false;
+
+}
+
+void PrintFinalResult (bool pass) {
+
+    cout << "______________________________________\n";
+    
+    if(pass) {
+
+        cout << "Final Result is Pass :-)\n";
+    }
+    else {
+
+        cout << "Final Result is Fail :-(\n";
+    
+    }
+
+}
+
+void PrintFinalmRestlGame (stInfoResultGame InfoResultGame) {
+
+    PrintFinalResult(InfoResultGame.Pass);
+
+    cout << "Number Of Question: " << InfoResultGame.NumberOfQuestion << endl;
+    cout << "Question Level: " << InfoResultGame.NameQuestionLevel << endl;
+    cout << "OpType: " << InfoResultGame.NameOpType << endl;
+    cout << "Number Of Right Answer: " << InfoResultGame.NumberOfRightAnswers << endl;
+    cout << "Number Of Wrong answer: " << InfoResultGame.NumberOfWrongAnswer << endl << endl; 
+
+    cout << "______________________________________\n";
+
+}
+bool TrueSum (short Sum, short AnswerUser) {
+
+   if (Sum == AnswerUser)
+   return true;
+   else
+   return false;
+
+}
+
+void TrueAnswer (bool Answer) {
+
+    if (Answer == true) {
+
+        system("color 2F");
+
+    }
+    else {
+
+         system("color 4F");
+    
+    }
+}
+
+int QuestionSum (enLevelGame Level ,enOperationType OperationType, short FirstNumber, short SecondNumber) {
+
+    short Sum = 0;
+   // Part Easy.
+   if (Level == enLevelGame::Easy && OperationType == enOperationType::Add) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == enLevelGame::Easy && OperationType == enOperationType::Subtraction) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == enLevelGame::Easy && OperationType == enOperationType::Multiplication) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == enLevelGame::Easy && OperationType == enOperationType::Division) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == enLevelGame::Easy && OperationType == enOperationType::MiX) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+  
+   // Part Medium.
+   else if (Level == enLevelGame::Medium && OperationType == enOperationType::Add) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+
+   else if (Level == enLevelGame::Medium && OperationType == enOperationType::Subtraction) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == enLevelGame::Medium && OperationType == enOperationType::Multiplication) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == enLevelGame::Medium && OperationType == enOperationType::Division) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == enLevelGame::Medium && OperationType == enOperationType::MiX) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+   }
+   // Part Hard.
+   else if (Level == enLevelGame::Hard && OperationType == enOperationType::Add) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == enLevelGame::Hard && OperationType == enOperationType::Subtraction) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == enLevelGame::Hard && OperationType == enOperationType::Multiplication) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == enLevelGame::Hard && OperationType == enOperationType::Division) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == enLevelGame::Medium && OperationType == enOperationType::MiX) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   // Part Mix.
+   else if (Level == (enLevelGame)Random_Number(1, 4) && enOperationType::Add) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == (enLevelGame)Random_Number(1, 4) && enOperationType::Subtraction) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == (enLevelGame)Random_Number(1, 4) && enOperationType::Multiplication) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == (enLevelGame)Random_Number(1, 4) && enOperationType::Division) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+   else if (Level == (enLevelGame)Random_Number(1, 4) && enOperationType::MiX) {
+
+    PrintQuestion(OperationType, FirstNumber, SecondNumber);
+    cin >> Sum;
+
+   }
+
+   return Sum;
+
+}
+
+int CalculationSum (enOperationType Op ,short FirstNumber, short SecondNumber) {
+
+    short Sum = 0;
+
+    switch (Op) {
+
+        case Add:
+        return Sum = FirstNumber + SecondNumber;
+        break;
+
+        case Subtraction:
+        return Sum = FirstNumber - SecondNumber;
+        break;
+
+        case Multiplication:
+        return Sum = FirstNumber * SecondNumber;
+        break;
+
+        case Division:
+        return Sum = FirstNumber / SecondNumber;
+        break;
+
+    }
+
+    return 1;
+
+
+
+}
+
+stInfoResultGame StartQuestion() {
+
+    short NumberOfRight = 0, NumberOfWrong = 0;
+
+    stInfoGame InfoGame;
+    stInfoResultGame InfoResultGame;
+
+    InfoGame.Question = HowToManyQus();
+    InfoGame.Level = ReadChiosUserLevel();
+    InfoGame.Operator = ReadChiosUserOperationType();
+
+
+    for (int QusNumber = 1; QusNumber <= InfoGame.Question; QusNumber++) {
+
+        cout << "Question [" << QusNumber << "/" << InfoGame.Question << "]\n\n" << endl;
+        
+        InfoGame.FirstNumber = NumberLevel(InfoGame.Level);
+        InfoGame.SecondNumber = NumberLevel(InfoGame.Level);
+       
+        InfoGame.Sum = CalculationSum(InfoGame.Operator, InfoGame.FirstNumber, InfoGame.SecondNumber);
+        InfoGame.AnswerUser =  QuestionSum(InfoGame.Level, InfoGame.Operator, InfoGame.FirstNumber, InfoGame.SecondNumber);
+        InfoGame.Answer = TrueSum(InfoGame.Sum, InfoGame.AnswerUser);
+        TrueAnswer(InfoGame.Answer);
+
+        if (InfoGame.Answer == true) {
+
+            ++NumberOfRight;
+        
+        }
+        else {
+
+            ++NumberOfWrong;
+        
+        }
+
+       
+    InfoResultGame.NumberOfQuestion = InfoGame.Question;
+    InfoResultGame.NameQuestionLevel = LevelName(InfoGame.Level);
+    InfoResultGame.NameOpType = OperatorTypeName(InfoGame.Operator);
+    InfoResultGame.NumberOfRightAnswers = NumberOfRight;
+    InfoResultGame.NumberOfWrongAnswer = NumberOfWrong;
+    InfoResultGame.Pass = PassOrFail(InfoResultGame.NumberOfRightAnswers, InfoResultGame.NumberOfWrongAnswer);
+
+    }
+    
+    return InfoResultGame;
+
+}
+
+void RestScreen () {
+
+    system("color 0F");
+    system("cls");
+    
+
+}
+
+void StartGame () {
+
+    char Again = 'y';
+
+    do {
+
+        RestScreen();
+        PrintFinalmRestlGame(StartQuestion());
+
+        cout << "Do you want to play again? Y/N?";
+        cin >> Again;  
+    
+    }
+    while (Again == 'y' || Again == 'Y');
+
+}
+
+
+int main () {
+
+    srand((unsigned)time(NULL));
+
+    StartGame();
+
+    return 0;
+
+}
---
+++ readme.md
@@ -0,0 +1,99 @@
+Math Game 🎮➗
+
+A simple console-based Math Game built using C++.
+The game generates random math questions with different difficulty levels and operation types.
+
+📌 Features
+Multiple difficulty levels:
+Easy
+Medium
+Hard
+Mix
+Multiple operation types:
+Addition
+Subtraction
+Multiplication
+Division
+Mix
+Random question generation
+Tracks:
+Correct answers
+Wrong answers
+Final result (Pass / Fail)
+Console color feedback:
+Green → Correct answer
+Red → Wrong answer
+Play again system
+🛠️ Technologies Used
+C++
+Standard Library:
+<iostream>
+<ctime>
+<cstdlib>
+▶️ How to Run
+Compile using g++
+g++ main.cpp -o MathGame
+Run
+./MathGame
+
+On Windows:
+
+MathGame.exe
+🎯 Game Flow
+Enter number of questions
+Choose difficulty level
+Choose operation type
+Solve the generated questions
+View final results
+Choose whether to play again
+📷 Example
+How Many Questions do you want to answer ? 5
+
+Enter Question Level
+[1] Easy
+[2] Med
+[3] Hard
+[4] Mix ? 1
+
+Enter Question Level
+[1] Addition
+[2] Subtraction
+[3] Multiplication
+[4] Division
+[5] Mix ? 1
+
+Question [1/5]
+
+5
+7 +
+
+________________
+12
+📊 Final Result Example
+______________________________________
+
+Final Result is Pass :-)
+
+Number Of Question: 5
+Question Level: Easy
+OpType: Addition
+Number Of Right Answer: 4
+Number Of Wrong answer: 1
+
+______________________________________
+📂 Project Structure
+MathGame/
+│
+├── main.cpp
+└── README.md
+🚀 Future Improvements
+Add timer for each question
+Add score system
+Save results to file
+Add sounds
+Improve question generation
+Support floating-point division
+Better UI design
+👨‍💻 Author
+
+Created by Ahmed Khemliche.
